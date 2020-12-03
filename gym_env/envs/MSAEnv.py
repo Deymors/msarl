f3027f6d87fa65624d7d8b35688ca7aa38ff7200
jiarshao <jia.r.shao@mail.mcgill.ca>
2020-12-03 17:30:20 -0500

    Create dirs for gym env, started setup

--- gym_env/envs/MSAEnv.py
+++ gym_env/envs/MSAEnv.py
@@ -5,6 +5,7 @@ from gym import error, spaces
 from gym import utils
 from gym.utils import seeding
 
+
 # gym.Env parent class: https://github.com/openai/gym/blob/master/gym/core.py
 
 
@@ -24,3 +25,7 @@ class MSAEnv(gym.Env):
     # abstract method from parent gym.Env
     def render(self, mode='human'):
         pass
+
+
+if __name__ == "__main__":
+    env = gym.make("MSAEnv-v0")
---
+++ gym_env/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_envs.envs.MSAGym import MSAEnv
