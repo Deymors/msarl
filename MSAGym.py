3b8fcfe76158f363b347c01ef92cfff5f921c242
jiarshao <jia.r.shao@mail.mcgill.ca>
2020-12-03 17:14:58 -0500

    base for gym env

---
+++ MSAGym.py
@@ -0,0 +1,26 @@
+import numpy as np
+import os
+import gym
+from gym import error, spaces
+from gym import utils
+from gym.utils import seeding
+
+# gym.Env parent class: https://github.com/openai/gym/blob/master/gym/core.py
+
+
+class MSAEnv(gym.Env):
+
+    def __init__(self):
+        pass
+
+    # abstract method from parent gym.Env
+    def step(self, action):
+        pass
+
+    # abstract method from parent gym.Env
+    def reset(self):
+        pass
+
+    # abstract method from parent gym.Env
+    def render(self, mode='human'):
+        pass
